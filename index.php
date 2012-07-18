3b1014b589e4d24c5c27029904aa76d2d9e794fa
Josep Viciana <josep@viciana.me>
2012-07-19 00:09:36 +0200

    Inicio maquinación. Estructura 1. Form / UI.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8">
+		<meta http-equiv="content-type" content="text/html;charset=utf-8">
 		<title>Devicelost 4</title>
 		<! Plugins y librer’as >
 		<script src="recursos/js/jquery.min.js" charset="utf-8"></script>
@@ -10,6 +10,27 @@
 		<link rel="stylesheet" href="recursos/css/main.css" type="text/css" charset="utf-8">
 	</head>
 	<body>
-	<p>Mira el c&oacute;digo en <a href="http://www.github.com/emmgfx/devicelost" target="_blank">GitHub</a>.</p>
+	
+	<div id="header">
+		<h1>Devicelost</h1>
+	</div>
+	
+	<div id="main">
+		
+		<form id="login">
+			<div class="heading">Iniciar sesi&oacute;n</div>
+			<div class="content">
+				<input type="text" placeholder="Usuario" class="input1" />
+				<input type="password" placeholder="Contrase&ntilde;a" class="input1" />
+				
+				<div class="cb"></div>
+					<input type="checkbox" name="mantener" id="mantener" class="fl" /> <label for="mantener" class="fl">Mantener sesi&oacute;n en este ordenador</label>
+					<input type="submit" value="Entrar" class="fr">
+				<div class="cb"></div>
+			</div>
+		</form>
+		
+	</div>
+	
 	</body>
 </html>
