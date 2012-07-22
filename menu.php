80dee58e9d3e923f785a02c2e2a12308d19543e5
Josep Viciana <josep@viciana.me>
2012-07-22 13:34:04 +0200

    Enlaces del menú header

--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
 <ul id="menu">
-	<li><a href="#">Sobre Devicelost</a></li>
-	<li><a href="#">Opciones</a></li>
+	<li><a href="http://www.github.com/emmgfx/Devicelost" target="_blank">C&oacute;digo fuente en GitHub</a></li>
+	<li><a href="https://github.com/emmgfx/Devicelost/issues" target="_blank">Avisar de un error</a></li>
 	<li><a href="login-logout.php" id="logout">Cerrar sesi&oacute;n</a></li>
 </ul>
